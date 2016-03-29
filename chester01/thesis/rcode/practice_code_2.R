4a50b5c7cb4ade10bc69a6dc3b9d925b3adc72ce
Hans Trautlein <hans@trautlein.com>
2016-03-29 14:08:43 -0700

    first commit, added files

---
+++ chester01/thesis/rcode/practice_code_2.R
@@ -0,0 +1,77 @@
+library(ggplot2)
+library(dplyr)
+
+list.files()
+#setwd("~/Insync/thesis/")
+
+d <- read.csv("~/Insync/thesis/data/SenateData002.csv")
+d <- d[,c("Cong.","ICPSR", "State.Code", "State", "Party", "Name", "X1st.Dim.", "X2nd.Dim.", "Role")]
+
+d <- dplyr::rename(d, cong = Cong., 
+                   icpsr = ICPSR,
+                   statecode = State.Code,
+                   state = State, 
+                   party = Party,
+                   name = Name, 
+                   ideo = X1st.Dim., 
+                   x2 = X2nd.Dim.,
+                   role = Role)
+names(d)
+
+start_cong <- 82
+
+d %>% # strips out 3rd party, before cong 60, make party variable D or R instead of 100 or 200
+  mutate(leader = ifelse(role == "", NA, "Y")) %>%
+  filter((party == 100 | party == 200) & cong >= start_cong) %>%
+  mutate(party2 = ifelse(party == 100, "D","R")) %>%
+  select(-party) %>%
+  rename(party = party2) -> d1
+
+#< Make party_cong_mean variable and 
+party_means <- data.frame(matrix(NA, nrow = 113, ncol = 0))
+party_means$D     <- rep(NA, 113)
+party_means$R     <- rep(NA, 113)
+party_means$R_del <- rep(NA, 113)
+party_means$D_del <- rep(NA, 113)
+
+for(i in (start_cong+1):113) {
+  party_means$D[i]  <- mean(dplyr::filter(d1, cong == i & party == "D")$ideo)
+  party_means$R[i]  <- mean(dplyr::filter(d1, cong == i & party == "R")$ideo)
+  party_means$D_del[i] <- party_means$D[i] - party_means$D[i-1]
+  party_means$R_del[i] <- party_means$R[i] - party_means$R[i-1]
+}
+
+d2 <- dplyr::mutate(d1, party_cong_mean = ifelse(party=="D", 
+                                                 party_means$D[cong], 
+                                                 party_means$R[cong]))
+
+
+d2 <- dplyr::mutate(d2, party_cong_mean_dif = ifelse(party=="D", 
+                                                 party_means$D_del[cong], 
+                                                 party_means$R_del[cong]))
+#>
+
+d2 <- dplyr::mutate(d2, personal_party_mean_dif = ifelse(party=="D", 
+                                                     party_means$D_del[cong] - ideo, 
+                                                     party_means$R_del[cong] - ideo))
+
+
+
+
+
+
+
+
+
+ggplot(dplyr::filter(d1, cong > 90), aes(cong, ideo, color = party)) + geom_point() +
+  scale_color_manual(values=c("blue", "red"))
+
+
+leahy <- dplyr::filter(d, icpsr == 14307)
+ggplot(leahy, aes(cong, ideo)) + geom_point()
+
+ggplot(dplyr::filter(d1, cong >= 110), aes(ideo, x2, color=party)) + geom_point() +
+  scale_color_manual(values=c("blue", "red"))
+
+cong113 <- dplyr::filter(d1, cong == 113)
+ggplot(cong113, aes(ideo, color = party)) + geom_histogram(binwidth = 0.025)
